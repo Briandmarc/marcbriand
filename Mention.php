5de6ed82c6082c91978f2478726c7ff9c08d902a
Briandmarc <marcbriand33@gmail.com>
2017-11-23 15:33:35 +0100

    add mention légales

---
+++ Mention.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" type="text/css" href="css/folio.css"/>
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css">
+	<meta name="description" content="Je m'appelle Marc Briand, j'étudie le web et le multimédia à l'IUT Bordeaux Montaigne." />
+<title>Marc-Briand.com</title>
+</head>
+	<body>
+		<header><!--
+			 --><nav>
+					<ul>
+						<li><a href="index.html">Home</li></a>
+						<li><a href="index.html#front_page">CV</li></a>
+						<li><a href="index.html#Portfolio">Portfolio</li></a>
+						<li><a href="mailto:marcbriand33@gmail.com.com">Contact</li></a>
+					</ul>
+				</nav>
+		</header>
+				<span>
+					<h1>Mention Légales</h1>
+						<h2>Par Marc BRIAND</h2>
+				</span>
+
+						<span class="line_folio"></span>
+								<div id="front_page_right">
+
+								</div>
+								<div id="front_page_left">
+									<div id="block_left">
+										<h2>Informations légales</h2>
+
+<p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site <a href="http://https://briandmarc.github.io/supersite.github.io//">https://briandmarc.github.io/supersite.github.io/</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
+<p><strong>Propriétaire</strong> : Marc Briand – étudiant – 33800<br />
+<strong>Créateur</strong>  : <a href="https://briandmarc.github.io/supersite.github.io/">Marc Briand</a><br />
+<strong>Responsable publication</strong> : Marc Briand – marcbriand33@gmail.com<br />
+Le responsable publication est une personne physique ou une personne morale.<br />
+<strong>Webmaster</strong> : Marc Briand – marcbriand33@gmail.com<br />
+<strong>Hébergeur</strong> : Github – San Francisco<br />
+Crédits : <br />
+Le modèle de mentions légales est offert par Subdelirium.com <a target="_blank" href="https://www.subdelirium.com/generateur-de-mentions-legales/" alt="generateur de mentions légales">Générateur de mentions légales</a></p>
+									</div>
+								</div>
+
+				<footer>
+						<p> Marc Briand - 2016 - Tous droits réservés - </font>
+							<a href="Mention.php"> Mentions légales</p>
+				</footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" type="text/javascript"></script>
+		<script src="jsMain/jquery.flexslider.js"></script>
+		<script type="text/javascript" charset="utf-8">
+		  $(window).load(function() {
+		    $('.flexslider').flexslider();
+		  });
+		</script>
+			</body>
+		</html>
